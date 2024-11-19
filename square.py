a5786d7c0d0f632f797f602080768bd9e26e3240
Nikita Belov <biban228@yahoo.com>
2024-11-20 00:07:58 +0300

    Update square.py

--- square.py
+++ square.py
@@ -3,8 +3,8 @@ def area(a):
         raise AssertionError("Сторона не может быть отрицательной")
     return a * a
 
+
 def perimeter(a):
     if a < 0:
         raise AssertionError("Сторона не может быть отрицательной")
     return 4 * a
-
