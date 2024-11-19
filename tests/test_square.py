bfc224d0b93b2c70d34e2ab01ac30dab601685a9
Nikita Belov <biban228@yahoo.com>
2024-11-19 23:52:19 +0300

    corrected code taking into account PEP 8 standards: test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -6,32 +6,32 @@ class TestSquare(unittest.TestCase):
     def test_area(self):
         side = 1
         res = area(side)
-        self.assertEqual(res,1)
+        self.assertEqual(res, 1)
 
     def test_perimeter(self):
         side = 1
         res = perimeter(side)
-        self.assertEqual(res,4)
+        self.assertEqual(res, 4)
 
     def test_area_zero(self):
         side = 0
         res = area(side)
-        self.assertEqual(res,0)
+        self.assertEqual(res, 0)
 
     def test_perimeter_zero(self):
         side = 0
         res = perimeter(side)
-        self.assertEqual(res,0)
+        self.assertEqual(res, 0)
 
     def test_area_neg(self):
         side = -1
-        res = area(side)
-        self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError):
+            area(side)
 
     def test_perimeter_neg(self):
         side = -1
-        res = area(side)
-        self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError):
+            perimeter(side)
 
 
 if __name__ == '__main__':
