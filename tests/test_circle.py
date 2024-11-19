0631b121c4a943c74c249283a7c80a72057dd12a
Nikita Belov <biban228@yahoo.com>
2024-11-19 23:51:00 +0300

    corrected code taking into account PEP 8 standards: test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,17 +1,17 @@
 import unittest
-from math import *
+from math import pi
 from circle import area, perimeter
 
 class TestCircle(unittest.TestCase):
     def test_area(self):
         radius = 1
         res = area(radius)
-        self.assertEqual(res,pi)
+        self.assertEqual(res, pi)
 
     def test_perimeter(self):
         radius = 1
         res = perimeter(radius)
-        self.assertEqual(res,2 * pi)
+        self.assertEqual(res, 2 * pi)
 
     def test_area_zero(self):
         radius = 0
@@ -25,13 +25,13 @@ class TestCircle(unittest.TestCase):
 
     def test_area_neg(self):
         radius = -1
-        res = area(radius)
-        self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError):
+            area(radius)
 
     def test_perimeter_neg(self):
         radius = -1
-        res = perimeter(radius)
-        self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError):
+            perimeter(radius)
 
 if __name__ == '__main__':
     unittest.main()
