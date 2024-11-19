7aa9c0a89641fb5418a7cb80653189977642fa49
Nikita Belov <biban228@yahoo.com>
2024-11-19 23:53:15 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -9,29 +9,29 @@ class TestTriangle(unittest.TestCase):
         self.assertEqual(res, 30)
 
     def test_area_zero(self):
-        x,y,z = 0, 0, 0
-        res = area(x,y,z)
+        x, y, z = 0, 0, 0
+        res = area(x, y, z)
         self.assertEqual(res, 0)
 
     def test_perimeter(self):
-        x,y,z = 5, 12, 13
-        res = perimeter(x,y,z)
+        x, y, z = 5, 12, 13
+        res = perimeter(x, y, z)
         self.assertEqual(res, 30)
 
     def test_perimeter_zero(self):
-        x,y,z = 0, 0, 0
+        x, y, z = 0, 0, 0
         res = perimeter(x, y, z)
         self.assertEqual(res, 0)
 
     def test_area_neg(self):
-        x,y,z = -5,-12,-13
-        res = area(x,y,z)
-        self.assertRaises(AssertionError)
+        x, y, z = -5, -12, -13
+        with self.assertRaises(AssertionError):
+            area(x, y, z)
 
     def test_perimeter_neg(self):
-        x,y,z = -5,-12,-13
-        res = perimeter(x,y,z)
-        self.assertRaises(AssertionError)
+        x, y, z = -5, -12, -13
+        with self.assertRaises(AssertionError):
+            perimeter(x, y, z)
 
 
 if __name__ == '__main__':
