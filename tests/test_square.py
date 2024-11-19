146fb83163ece268fdf132b2b71aa7c1481b5a79
Nikita Belov <biban228@yahoo.com>
2024-11-19 20:55:41 +0300

    Add files via upload

---
+++ tests/test_square.py
@@ -0,0 +1,38 @@
+import unittest
+from square import area, perimeter
+
+
+class TestSquare(unittest.TestCase):
+    def test_area(self):
+        side = 1
+        res = area(side)
+        self.assertEqual(res,1)
+
+    def test_perimeter(self):
+        side = 1
+        res = perimeter(side)
+        self.assertEqual(res,4)
+
+    def test_area_zero(self):
+        side = 0
+        res = area(side)
+        self.assertEqual(res,0)
+
+    def test_perimeter_zero(self):
+        side = 0
+        res = perimeter(side)
+        self.assertEqual(res,0)
+
+    def test_area_neg(self):
+        side = -1
+        res = area(side)
+        self.assertRaises(AssertionError)
+
+    def test_perimeter_neg(self):
+        side = -1
+        res = area(side)
+        self.assertRaises(AssertionError)
+
+
+if __name__ == '__main__':
+    unittest.main()
